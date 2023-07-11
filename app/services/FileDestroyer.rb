2e2e5649038d2147360991a3decd9ed71d6dd985
Arthur_Macedo <arthurmacedo01@gmail.com>
2023-07-11 11:30:19 -0300

    test grover

--- app/services/FileDestroyer.rb
+++
@@ -1,9 +0,0 @@
-class FileDestroyer
-  def self.call(file_path)
-    new().call(file_path)
-  end
-
-  def call(file_path)
-    File.delete(file_path) if file_path && File.exist?(file_path)
-  end
-end
